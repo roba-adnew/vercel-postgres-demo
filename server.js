a844d03f2fbabacf95da5d13a9977acd5de22629
roba <10408936+roba-adnew@users.noreply.github.com>
2024-09-06 11:42:38 -0400

    Removing cors as its unnecessary in this example

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+async function getUsers() {
+    const users = await prisma.user.findMany();
+    return users
+}
+
+app.get('/',
+    async (req, res, next) => {
+        const users = await getUsers();
+        res.send(`<h1>here are the users: ${users[0].name}</h1>`)
+    }
+)
+
+app.listen(3000)
+
+module.exports = app;
