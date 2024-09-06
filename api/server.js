73fbf3e5951725a9157cdb56d554d1adcbe6488d
roba <10408936+roba-adnew@users.noreply.github.com>
2024-09-06 11:57:06 -0400

    readding cors

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const { PrismaClient } = require('@prisma/client');
 
@@ -6,6 +7,10 @@ const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:4000',
+    credentials: true
+}))
 
 async function getUsers() {
     const users = await prisma.user.findMany();
@@ -13,7 +18,7 @@ async function getUsers() {
 }
 
 app.get('/',
-    async (req, res, next) => {
+    async (req, res) => {
         const users = await getUsers();
         res.send(`<h1>here are the users: ${users[0].name}</h1>`)
     }
